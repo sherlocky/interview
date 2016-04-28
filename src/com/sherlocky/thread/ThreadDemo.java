629927028e98697dac245890f1e989934ff24258
Sherlocky <y836097668@126.com>
2016-04-28 23:45:05 +0800

    thread和runnable区别

---
+++ src/com/sherlocky/thread/ThreadDemo.java
@@ -0,0 +1,24 @@
+package com.sherlocky.thread;
+
+/** 
+
+* @author Sherlocky
+* @date 2016年4月28日
+*/
+public class ThreadDemo {
+	public static void main(String[] args) {
+//		MyThread mt1 = new MyThread("线程t-a");  
+//		MyThread mt2 = new MyThread("线程t-b"); 
+//		mt1.start();  
+//		mt2.run();  
+		
+//		MyRunnable mr1 = new MyRunnable("线程r-a");  
+//		MyRunnable mr2 = new MyRunnable("线程r-b");  
+//		new Thread(mr1).start();  
+//		new Thread(mr2).start();  
+		
+//		MyRunnable mr3 = new MyRunnable("线程r-c");  
+//		new Thread(mr3, "c01").start();  
+//		new Thread(mr3, "c02").start();  
+	}
+}
