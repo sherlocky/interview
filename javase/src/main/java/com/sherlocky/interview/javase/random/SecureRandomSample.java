fad771a5b6d237c6556b6419acb226723171fd88
Sherlocky <y836097668@126.com>
2019-06-28 16:33:46 +0800

    多线程使用ThreadLocalRandom(jdk7+)

--- javase/src/main/java/com/sherlocky/interview/javase/random/SecureRandomSample.java
+++ javase/src/main/java/com/sherlocky/interview/javase/random/SecureRandomSample.java
@@ -37,6 +37,13 @@ public class SecureRandomSample {
      * 也就是说，两个种子数相同的Random对象，生成的随机数字完全相同
      */
     private static void testRandom() {
+        /**
+         * 【推荐】 避免 Random 实例被多线程使用，虽然共享该实例是线程安全的，但会因竞争同一 seed 导致的性能下降。
+         *
+         * 说明：Random 实例包括 java.util.Random 的实例或者 Math.random()的方式。
+         *
+         * 【正例】：在 JDK7 之后，可以直接使用 API ThreadLocalRandom，而在 JDK7 之前，需要编码保证每个线程持有一个实例。
+         */
         Random random1 = new Random(10000);
         Random random2 = new Random(10000);
 
